3a0e794de8678981663870a4a4437595d0394a1d
Md. Razikul Islam Joni <razikuljoni@gmail.com>
2024-12-21 16:18:04 +0600

    Initialize update-nextjs15

---
+++ src/app/[locale]/(auth)/layout.tsx
@@ -0,0 +1,39 @@
+import { AppConfig } from '@/utils/AppConfig';
+import { enUS, frFR } from '@clerk/localizations';
+import { ClerkProvider } from '@clerk/nextjs';
+
+export default async function AuthLayout(props: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await props.params;
+  let clerkLocale = enUS;
+  let signInUrl = '/sign-in';
+  let signUpUrl = '/sign-up';
+  let dashboardUrl = '/dashboard';
+  let afterSignOutUrl = '/';
+
+  if (locale === 'fr') {
+    clerkLocale = frFR;
+  }
+
+  if (locale !== AppConfig.defaultLocale) {
+    signInUrl = `/${locale}${signInUrl}`;
+    signUpUrl = `/${locale}${signUpUrl}`;
+    dashboardUrl = `/${locale}${dashboardUrl}`;
+    afterSignOutUrl = `/${locale}${afterSignOutUrl}`;
+  }
+
+  return (
+    <ClerkProvider
+      localization={clerkLocale}
+      signInUrl={signInUrl}
+      signUpUrl={signUpUrl}
+      signInFallbackRedirectUrl={dashboardUrl}
+      signUpFallbackRedirectUrl={dashboardUrl}
+      afterSignOutUrl={afterSignOutUrl}
+    >
+      {props.children}
+    </ClerkProvider>
+  );
+}
